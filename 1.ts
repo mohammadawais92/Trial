779e2d4aca787268f4d2ddb73691f4e07480f21d
Awais <mushtaqawais@gmail.com>
2022-09-23 23:59:00 +0500

    first commit

---
+++ 1.ts
@@ -0,0 +1,12 @@
+// User defined objects
+// Arrays, Functions, Classes, Tuples, Interface, Enum
+
+// var things;
+// function message():string {
+
+//     things = "pak"
+//     console.log(abc)
+//     return things
+// }
+// message()
+
